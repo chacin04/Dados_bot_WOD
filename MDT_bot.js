1be2c522707743c2912ad015d7202439f0f9c912
chacin04 <angelgabriel4.ac@gmail.com>
2024-07-08 11:46:37 -0400

    feat ( sqlite - iniciativa )

--- MDT_bot.js
+++ MDT_bot.js
@@ -1,38 +1,119 @@
-const TeleBot = require('telebot');
-const { ID } = require('./config.js')
+const TeleBot = require("telebot");
+const { ID } = require("./config.js");
 const bot = new TeleBot(ID);
-const {tiradaNormal} = require('./controllers/tiradaNormal.js')
-const{horaActualVenezuela} =require('./controllers/horaKeiber.js')
-
+const { tiradaNormal } = require("./controllers/tiradaNormal.js");
+const { horaActualVenezuela } = require("./controllers/horaKeiber.js");
+const {
+    insertarDatos,
+    mostrarRegistros,
+    eliminarIniciativas,
+} = require("./lib/sqlite.js");
 
 // INICIATIVA
-bot.on([/^\/i (.+)$/, /^\/I (.+)$/], (msg, props) => {
+bot.on([/^\/i (.+)$/, /^\/I (.+)$/], async (msg, props) => {
     let dato = props.match[1];
     let numeros = dato.split(" ");
     let numero = numeros[0];
+    let personaje = numeros[1];
     let mod = parseInt(numero);
-    var x = Math.floor(Math.random() * 10) + 1
-    if (mod >= 0 && mod < 30) {
-        let total = x + mod
-        var men = ("      🧚🏻🧙🏼MUNDO DE TINIEBLAS 👻🧛‍♀️" + "\n--INICIATIVA--" + "\nPersonaje = " + mod + "\nDado🎲 = " + x + "\nTOTAL = " + total + "⚡")
-        return bot.sendMessage(msg.chat.id, men, { replyToMessage: msg.message_id });
-    } else {
-        return bot.sendMessage(msg.chat.id, "NO PERMITIDO 😡", { replyToMessage: msg.message_id });
-    }
+    var x = Math.floor(Math.random() * 10) + 1;
+    if (mod >= 0 && mod < 30 && personaje) {
+        let total = x + mod;
 
+        const respuestaInsert = await insertarDatos(
+            personaje.toLowerCase(),
+            total,
+            mod
+        );
+        if (respuestaInsert) {
+            var men =
+                "      🧚🏻🧙🏼MUNDO DE TINIEBLAS 👻🧛‍♀️" +
+                "\n--INICIATIVA--" +
+                "\nPersonaje = " +
+                mod +
+                " " +
+                personaje.toLowerCase() +
+                "\nDado🎲 = " +
+                x +
+                "\nTOTAL = " +
+                total +
+                "⚡";
+            return bot.sendMessage(msg.chat.id, men, {
+                replyToMessage: msg.message_id,
+            });
+        } else {
+            return bot.sendMessage(msg.chat.id, "Error Guardado 😡", {
+                replyToMessage: msg.message_id,
+            });
+        }
+    } else {
+        return bot.sendMessage(msg.chat.id, "NO PERMITIDO 😡", {
+            replyToMessage: msg.message_id,
+        });
+    }
+});
+
+bot.on(["/Print", "/print"], async (msg, props) => {
+    const resultadosGetDb = await mostrarRegistros();
+    if (mostrarRegistros) {
+        const resultadosMensaje = resultadosGetDb
+            .map(({ name, numero_iniciativa }) => {
+                return `${name} ${numero_iniciativa}`;
+            })
+            .join("\n");
+        const eliminarTabla = await eliminarIniciativas();
+        if (eliminarTabla) {
+            const mensaje = `🧚🏻🧙🏼MUNDO DE TINIEBLAS 👻🧛‍♀️\n--INICIATIVA LIST--\n\n${resultadosMensaje}\n\nLanta iniciativa cuando termines turno`;
+            return bot.sendMessage(msg.chat.id, mensaje, {
+                replyToMessage: msg.message_id,
+            });
+        }else{
+            return bot.sendMessage(msg.chat.id, "Error DATOS Eliminacion😡", {
+                replyToMessage: msg.message_id,
+            });
+        }
+    }else{
+        return bot.sendMessage(msg.chat.id, "Error DATOS 😡", {
+            replyToMessage: msg.message_id,
+        });
+    }
 });
 
 //  TIRADA DE DADOS
-bot.on([/^\/wod (.+)$/, /^\/Wod (.+)$/, /^\/wOd (.+)$/, /^\/woD (.+)$/, /^\/WOD (.+)$/, /^\/WOd (.+)$/, /^\/wOD (.+)$/, /^\/WoD (.+)$/], (msg, props) => {
-    tiradaNormal(msg,props,false)
+bot.on(
+    [
+        /^\/wod (.+)$/,
+        /^\/Wod (.+)$/,
+        /^\/wOd (.+)$/,
+        /^\/woD (.+)$/,
+        /^\/WOD (.+)$/,
+        /^\/WOd (.+)$/,
+        /^\/wOD (.+)$/,
+        /^\/WoD (.+)$/,
+    ],
+    (msg, props) => {
+        tiradaNormal(msg, props, false);
+    }
+);
+
+bot.on(
+    [
+        /^\/pro (.+)$/,
+        /^\/PRO (.+)$/,
+        /^\/Pro (.+)$/,
+        /^\/pRo (.+)$/,
+        /^\/prO (.+)$/,
+        /^\/PRo (.+)$/,
+        /^\/pRO (.+)$/,
+        /^\/PrO (.+)$/,
+    ],
+    (msg, props) => {
+        tiradaNormal(msg, props, true);
+    }
+);
+
+bot.on(["/h", "/H"], (msg, props) => {
+    horaActualVenezuela(msg, props);
 });
-
-bot.on([/^\/pro (.+)$/,/^\/PRO (.+)$/,/^\/Pro (.+)$/,/^\/pRo (.+)$/,/^\/prO (.+)$/,/^\/PRo (.+)$/,/^\/pRO (.+)$/,/^\/PrO (.+)$/], (msg,props)=>{
-    tiradaNormal(msg,props,true)
-})
-
-bot.on(['/h','/H'],(msg,props)=>{
-    horaActualVenezuela(msg,props)
-})
 
 bot.start();
