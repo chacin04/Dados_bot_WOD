e2fbedea0dff2d406e1aa88f146af32bb530a903
chacin04 <angelgabriel4.ac@gmail.com>
2025-04-13 19:14:13 -0400

    fix ( v5 ) : la tirada es , dados totales espacio dados especiales

--- controllers/tiradav5.js
+++ controllers/tiradav5.js
@@ -6,13 +6,8 @@ const tiradav5 = (msg, props) => {
     let datos = props.match[1];
     let numeros = datos.split(" ");
     let cantidad = numeros[0];
-    let copiaCantidad = [...cantidad];
     let dadosEspeciales = isNaN(Number(numeros[1])) ? 0 : Number(numeros[1])
-    let difi = 6
     let dadosResultados = [];
-    let Cexitos = 0;
-    let botchParametro = 0;
-    let cantidadDeUnos = 0;
     
     if (cantidad >= 0 && cantidad <= 30) {
 
@@ -20,40 +15,29 @@ const tiradav5 = (msg, props) => {
             return dadosResultados.push(entrada)
         }
 
-        while (cantidad > 0) {
+        const resultadoCompleto = Array.from({length:cantidad},(_,index)=>{
             var x = Math.floor(Math.random() * 10) + 1
-            resultado({
-                dado:x,
-                type:"n"
-            })
-            
-            cantidad = cantidad - 1
-        }
-
-        while (dadosEspeciales > 0) {
-            var x = Math.floor(Math.random() * 10) + 1
-            resultado({
-                dado:x,
-                type:"e"
-            })
-            
-            dadosEspeciales = dadosEspeciales - 1
-        }
-
-        const cantidadExitos = (arrayDados)=>{
-            const checkCritical = arrayDados.filter((itemNumero,index)=>{
-                return itemNumero.dado == 10
-            }).length
-        }
+            if( index+1 <= dadosEspeciales){
+                return {
+                    dado:x,
+                    type:"e"
+                }
+            }else{
+                return {
+                    dado:x,
+                    type:"n"
+                }
+            }
+        })
 
         const mensajeEncabezado = "      🧚🏻🧙🏼MUNDO DE TINIEBLAS V5👻🧛‍♀️"
             
         //  ESTO YA ES EL TEXTO 
         var mensajeCompletado = (mensajeEncabezado + "\n RESULTADO:  \n" + 
-            dadosResultados.filter(i=>i.type=="n").map(itemDado=>{
+            resultadoCompleto.filter(i=>i.type=="n").map(itemDado=>{
                 return (itemDado.dado == 10)? "🔟" :((itemDado.dado > 5) ? "✅" : "🟩")
-            })+"\n --------------------------------- \n"+ 
-            dadosResultados.filter(i=>i.type=="e").map(itemDado=>{
+            })+"\n --------------------------------------- \n"+ 
+            resultadoCompleto.filter(i=>i.type=="e").map(itemDado=>{
                 return (itemDado.dado == 10)? "🔟" :((itemDado.dado == 1)? "❌" : ((itemDado.dado > 5) ? "✅" : "🟧"))
             })).replaceAll(",", " ");
             // TERMINA EL TEXTO
